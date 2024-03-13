d7efc77fe021c423087b04e1a1fdb972e58b2d77
rubiyadav <rubiflyeis@gmail.com>
2024-03-13 19:24:11 +0530

    13 march 2024 07:24 pm

--- model/Auth/transactionModel.js
+++ model/Auth/transactionModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const transactionSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.ObjectId,
-        ref: "user",
+        ref: "User",
     },
     id: {
         type: String,
